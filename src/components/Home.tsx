5d60a2b30559db34633ac2697405c0d77aa2cf8d
Arslan <arslanjs.dev@gmail.com>
2023-06-22 12:16:24 +0500

    Add GoogleIcon

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,18 @@
+import { Button, Center, Heading, Text } from "@chakra-ui/react";
+import GoogleIcon from "./ui/GoogleIcon";
+
 function Home() {
-    return <p>You are at Home</p>
+  return (
+    <Center h="100vh" style={{ flexDirection: "column" }}>
+      <Heading>Welcome To Home</Heading>
+      <Text color={"gray"} mb={5}>
+        Signin to continue
+      </Text>
+      <Button leftIcon={<GoogleIcon width={20} />} variant="outline">
+        Signin With Google
+      </Button>
+    </Center>
+  );
 }
 
 export default Home;
